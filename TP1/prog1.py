622c9f9a7075731755046045fd959bdc2b702c6a
user <user@debian11.myguest.virtualbox.org>
2024-09-03 12:32:42 +0200

    modif code pour trouver le carré en while true

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,2 +1,9 @@
-print("Hello, World!")
-
+while True:
+    try:
+        nombre = float(input("Entrez un nombre (ou 'q' pour quitter) : "))
+        if nombre == 'q':
+            break
+        carre = nombre * nombre
+        print("Le carré de", nombre, "est", carre)
+    except ValueError:
+        print("Veuillez entrer un nombre valide.")
