bfbf79e83fe71751ad70336bc9e909b8998dbdff
Exwyre <160131687+Exwyre@users.noreply.github.com>
2024-09-16 17:23:07 +0200

    Update fonctions.py Questions 5

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,5 +1,18 @@
-def puissance(a, b):
-	if not type(a) is int or not type(b) is int:
-		raise TypeError("Only integers are allowed")
-	return a**b
-
+def puissance(a,b):
+   if not type(a) is int or not type(b) is int:
+      raise TypeError("Only integers are allowed")
+   elif a==0:
+      if b > 0:
+          return 1
+      else:
+         return False
+   elif b != 0:
+      result=1
+      for i in range(abs(b)):
+         result *= a
+      if a < 0 and b < 0:
+         return 1/result
+      else:
+         return result
+   else:
+      return 1
