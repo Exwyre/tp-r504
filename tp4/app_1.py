091481ef3eceef2bcf647c56986f129e13fa9a87
Exwyre <exwyre@gmail.com>
2025-01-06 17:11:13 +0100

    TP4 regex terminer

--- tp4/app_1.py
+++ tp4/app_1.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import mysql.connector
+import re
 
 app = Flask(__name__)
  
@@ -16,13 +17,34 @@ db_config = {
 conn = mysql.connector.connect(**db_config)
 cursor = conn.cursor() 
 
-@app.route('/newuser/',methods = ['POST', 'GET'])
-def saisie():
+@app.route("/form")
+def monForm():
+	return render_template("form.html")
+
+
+@app.route('/newuser',methods = ['POST', 'GET'])
+def newuser():
+	patt1=".{6,}"
+	patt2=".*[0-9].*"
+	patt3=".*[a-z].*"
+	patt4=".*[A-Z].*"
+	patt5=".*(#|%|\{|\}|@).*"
 	if request.method == 'POST':
-		res = request.form.get( "lname" )
-		return res.text()
-		
-#		return render_template("result.html", result=result, app_version=app_version
+		res = request.form.get( "regex" )
+		if re.fullmatch( patt1, res ) == None:
+			return( "Echec sur la condition 1" )
+		if re.fullmatch( patt2, res ) == None:
+			return( "Echec sur la condition 2" )
+		if re.fullmatch( patt3, res ) == None:
+			return( "Echec sur la condition 3" )
+		if re.fullmatch( patt4, res ) == None:
+			return( "Echec sur la condition 4" )
+		if re.fullmatch( patt5, res ) == None:
+			return( "Echec sur la condition 5" )
+		else:
+			return res
+
+
 @app.route('/')
 def index():
     # Sample query
